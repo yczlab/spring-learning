956ad95b4130f8d97602eac2df1f41d786c5ab41
yangchongzhi <2685733832@qq.com>
2020-03-06 22:23:43 +0800

    泛型依赖注入 update

--- ioc/src/com/yczlab/spring/beans/generic/di/UserService.java
+++ ioc/src/com/yczlab/spring/beans/generic/di/UserService.java
@@ -1,9 +1,15 @@
 package com.yczlab.spring.beans.generic.di;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 //父类没有加入Spring的自动管理，子类使用@Service注解加入Spring的自动管理
 @Service
 public class UserService extends BaseService<User> {
-
+    //继承@AutoWired注解后，会自动配置好BaseRepository<User>类型的实例Bean
+    //相当于如下代码：
+    /*
+    @Autowired
+    protected BaseRepository<User> repository;
+    **/
 }
