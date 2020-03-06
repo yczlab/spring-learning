97c00254201856ad6177ff121c1f19414f213479
yangchongzhi <2685733832@qq.com>
2020-03-06 21:55:29 +0800

    通过FactoryBean来配置bean

---
+++ ioc/src/com/yczlab/spring/beans/factorybean/Main.java
@@ -0,0 +1,14 @@
+package com.yczlab.spring.beans.factorybean;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans-beanfactory.xml");
+
+        Car car = (Car) context.getBean("car");
+        System.out.println(car);
+
+    }
+}
