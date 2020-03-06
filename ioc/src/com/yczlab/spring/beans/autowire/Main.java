7f7b68776fbe7fd48da769414101d315a6b38881
yangchongzhi <2685733832@qq.com>
2020-03-06 21:21:59 +0800

    Bean的自动装配autowire属性

---
+++ ioc/src/com/yczlab/spring/beans/autowire/Main.java
@@ -0,0 +1,14 @@
+package com.yczlab.spring.beans.autowire;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans-autowire.xml");
+        Person person = (Person) context.getBean("person");
+        System.out.println(person);
+
+    }
+}
