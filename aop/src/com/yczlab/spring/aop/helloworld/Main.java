2d212f1b857751235d462283ae41e62e9aa95605
yangchongzhi <2685733832@qq.com>
2020-03-07 16:09:32 +0800

    Spring_AOP的相关基础术语

--- aop/src/com/yczlab/spring/aop/helloworld/Main.java
+++ aop/src/com/yczlab/spring/aop/helloworld/Main.java
@@ -16,6 +16,11 @@ public class Main {
         //通过使用动态代理的方式写入日志信息
         ArithmeticCalculator target = new ArithmeticCalculatorImpl();
         ArithmeticCalculator proxy = new ArithmeticCalculatorLoggingProxy(target).getLoggingProxy();
+
+        //打印代理对象的信息
+        System.out.println(proxy.getClass().getTypeName());
+        System.out.println(proxy.getClass().getName());
+
         result = proxy.add(1, 2);
         System.out.println("-->" + result);
         result = proxy.div(4, 2);
